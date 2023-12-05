aa7815d7c51c4aaf06e5eeb3bbd956f7aff47e68
Aksh-007 <akshaybarapatre54@gmail.com>
2023-12-05 09:15:27 +0530

    Feat - added middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,25 +1,32 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+ 
 
-// This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  const path = request.nextUrl.pathname;
-  const isPublicpath = path === "/login" || path === "/signup";
+  const path = request.nextUrl.pathname
 
-  //   extract token
-  //   ? optional chaning   
-  const token = request.cookies.get("token")?.value || "";
+  const isPublicPath = path === '/login' || path === '/signup' || path === '/verifyemail'
 
-  //based on variable we should access path
-  if (isPublicpath && token) {
-    return NextResponse.redirect(new URL("/", request.nextUrl));
+  const token = request.cookies.get('token')?.value || ''
+
+  if(isPublicPath && token) {
+    return NextResponse.redirect(new URL('/', request.nextUrl))
   }
-  if (!isPublicpath && !token) {
-    return NextResponse.redirect(new URL("/login", request.nextUrl));
+
+  if (!isPublicPath && !token) {
+    return NextResponse.redirect(new URL('/login', request.nextUrl))
   }
+    
 }
 
+ 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ["/", "/profile", "/login", "/signup"],
-};
+  matcher: [
+    '/',
+    '/profile',
+    '/login',
+    '/signup',
+    '/verifyemail'
+  ]
+}
